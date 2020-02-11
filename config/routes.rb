463082bd5a90e11beac505212d1df413d50ca4d4
sybileduarte <sybile.duarte@edhec.com>
2020-02-11 18:56:08 +0100

    Added the edit, update and delete functionnalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/new',             to: 'tasks#new', as: :new_task
+  get 'tasks',                 to: 'tasks#index'
+  get 'tasks/:id',             to: 'tasks#show', as: :task
+  post 'tasks',                to: 'tasks#create'
+  get '/tasks/:id/edit',       to: 'tasks#edit', as: :task_edit
+  patch 'tasks/:id',           to: 'tasks#update'
+  delete '/tasks/:id/destroy', to: 'tasks#destroy', as: :task_destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
